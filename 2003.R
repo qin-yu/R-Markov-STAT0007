ce2b0c3d2a6ec7735ac4aeca21c74352bae61a5b
Qin Yu <qin.yu.15@ucl.ac.uk>
2018-01-24 16:40:22 +0000

    V1.2 Week3-Lecture1 Exercise2

--- 2003.R
+++ 2003.R
@@ -5,6 +5,7 @@
 Sys.setenv(LANG = "en")
 library("markovchain")
 library("MASS") # Now purely for converting decimal to fraction
+print("----------------------------------------------------")
 
 printFraction <- function(mc){
     # To print the transition matrix in mc, a markovchain, with entries in fraction
@@ -32,32 +33,74 @@ printFraction(HIVmc)
 initialState <- c(0.7, 0.2, 0.1)
 print("The initial state has been set as: ")
 print(initialState)
+print("----------------------------------------------------")
 
 
-# This transition matrix appears in both slide 2 and exercise 2:
+# Exercise Sheet 2:
 cw2matrix = matrix(c(1/2, 1/2, 0, 0,
                      1, 0, 0, 0,
                      0, 1/2, 1/3, 1/6,
-                     0, 0, 0, 1), byrow=TRUE, nrow=4)
+                     0, 0, 0, 1), byrow = TRUE, nrow = 4)
 cw2mc = new("markovchain",
-            byrow=TRUE,
+            byrow = TRUE,
             transitionMatrix = cw2matrix,
             name = "CW2 Q1")
+cw2q1matrix = cw2matrix
+cw2q1 = cw2mc
 print("The setting of CW2 Q1")
-printFraction(cw2mc)
+printFraction(cw2q1)
+
+cw2q2matrix = matrix(c(1/6, 1/6, 1/6, 1/6, 1/6, 1/6,
+                       0, 1/3, 1/6, 1/6, 1/6, 1/6,
+                       0, 0, 1/2, 1/6, 1/6, 1/6,
+                       0, 0, 0, 2/3, 1/6, 1/6,
+                       0, 0, 0, 0, 5/6, 1/6,
+                       0, 0, 0, 0, 0, 1), byrow = TRUE, nrow = 6)
+cw2q2 = new("markovchain",
+            byrow = TRUE,
+            transitionMatrix = cw2q2matrix,
+            name = "CW2 Q2")
+print("The setting of CW2 Q2")
+printFraction(cw2q2)
+
+cw2q3matrix = matrix(c(1/4, 0, 1/2, 1/4,
+                     0, 1/5, 0, 4/5,
+                     0, 1, 0, 0,
+                     1/3, 1/3, 0, 1/3), byrow = TRUE, nrow = 4)
+cw2q3 = new("markovchain",
+            byrow = TRUE,
+            transitionMatrix = cw2q3matrix,
+            name = "CW2 Q3")
+print("The setting of CW2 Q3")
+printFraction(cw2q3)
+print("----------------------------------------------------")
 
 
-plotHIV <- function(n, t0, t){
-    # e.g. call plotHIV(20, "high", 19)
+
+plotHIV <- function(n, t0){
+    # Notice that the initial state is not on the output graph.
+    # e.g. call plotHIV(20, "low")
     simulated_data <- rmarkovchain(n = n, object = HIVmc, t0 = t0)
     print(simulated_data)
     numeric_simulated_data <- (as.numeric(simulated_data == "low")
         + as.numeric(simulated_data == "medium") * 2
         + as.numeric(simulated_data == "high") * 3)
-    t <- 0:t 
+    t <- 1:n
     plot(t, numeric_simulated_data,
          yaxt="n", type="o", ylab="State", xlab="Time",
          main="Simulation of the HIV Markov chain", col="blue" )
 }
 
 
+plotMarkov <- function(object, t0, n){
+    # To use this method make sure that the Markov chain has no state name.
+    # e.g. call plotHIV(20, "high", 19)
+    simulated_data <- rmarkovchain(n = n, object = object, t0 = t0)
+    print(simulated_data)
+    numeric_simulated_data <- as.numeric(simulated_data)
+    t <- 1:n
+    plot(t, numeric_simulated_data,
+         yaxt="n", type="o", ylab="State", xlab="Time",
+         main="Simulation of Markov chain", col="blue" )
+    axis(side=2, at=states(object), labels=TRUE)
+}
